d851fc194c75698bf96f2254359c4aa3e822706c
Zachary Doll <zack.j.doll@gmail.com>
2026-05-01 15:56:01 -0700

    darkMode? is set before screen is displayed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script dangerouslySetInnerHTML={{ __html: `(function(){try{if(window.matchMedia('(prefers-color-scheme: dark)').matches)document.documentElement.classList.add('dark')}catch(e){}})();` }} />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
